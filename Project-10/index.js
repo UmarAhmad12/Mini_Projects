c9cee177fb6406ac98d16db1f32b2ba04caab5ed
UmarAhmad12 <umara6274@gmail.com>
2023-08-11 12:03:07 +0530

    Added some logics

--- Project-10/index.js
+++ Project-10/index.js
@@ -8,3 +8,20 @@ const passwordRangeInputEl = document.getElementById("passRangeInput");
 const passRangeValueEl = document.getElementById("passRangeValue");
 const genBtn = document.getElementById("genBtn");
 const passwordEl = document.getElementById("password");
+
+const generatePassword = (passLength) => {
+    const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    const upperCaseLetters = isUpperCase ? "ABCDEFGHIJKLMNOPQRSTUVWXYZ" : "";
+    const numbers = isNumbers ? "0123456789" : "";
+    const symbols = isSymbols ? "!@#$%^&*()_+" : "";
+  
+    const passwordChar = lowerCaseLetters + upperCaseLetters + numbers + symbols;
+  
+    let password = "";
+  
+    for (let i = 0; i < passLength; i++) {
+      const charIndex = Math.floor(Math.random() * passwordChar.length);
+      password += passwordChar[charIndex];
+    }
+    return password;
+  };
