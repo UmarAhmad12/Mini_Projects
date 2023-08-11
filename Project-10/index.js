03013e9eb49f9c5edd5f5b82754eead312ebad12
UmarAhmad12 <umara6274@gmail.com>
2023-08-11 12:01:30 +0530

    Started working on js file

---
+++ Project-10/index.js
@@ -0,0 +1,10 @@
+let passwordLength = 8;
+
+let isUpperCase = false;
+let isNumbers = false;
+let isSymbols = false;
+
+const passwordRangeInputEl = document.getElementById("passRangeInput");
+const passRangeValueEl = document.getElementById("passRangeValue");
+const genBtn = document.getElementById("genBtn");
+const passwordEl = document.getElementById("password");
